4be195146e3e36c4417001b96a16cee995a0e770
Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
2024-03-18 22:46:01 +0100

    solves second part of the day one problem

--- src/java/day01/Main.java
+++ src/java/day01/Main.java
@@ -2,11 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class Main {
+  private static final Map<String, Integer> map = Map.of("one", 1, "two", 2, "three", 3, "four", 4,
+      "five", 5, "six", 6, "seven", 7, "eight", 8, "nine", 9);
+
   public static void main(String[] args) {
     final var lines = readFromInputFile("./input.txt");
     final var result = calculateCalibration(lines);
@@ -31,8 +33,7 @@ public class Main {
 
   private static Integer calculateCalibration(ArrayList<String> lines) {
     return lines.stream().mapToInt(line -> {
-      final var digits = line.chars().filter(Character::isDigit)
-          .mapToObj(Character::getNumericValue).collect(Collectors.toList());
+      final var digits = getDigits(line);
       final var first = digits.stream().findFirst();
       final var last = digits.stream().reduce((f, second) -> second);
 
@@ -40,11 +41,45 @@ public class Main {
     }).sum();
   }
 
+  private static ArrayList<Integer> getDigits(String line) {
+    final var result = new ArrayList<Integer>();
+    int i = 0;
+
+    while (i < line.length()) {
+      final var c = line.charAt(i);
+
+      if (Character.isDigit(c)) {
+        result.add(Character.getNumericValue(c));
+      } else {
+        final var word = getWordInMap(line, i);
+
+        if (word != null) {
+          result.add(map.get(word));
+        }
+      }
+
+      i++;
+    }
+
+    return result;
+  }
+
+  private static String getWordInMap(String line, int startIndex) {
+    for (int i = startIndex + 1; i <= line.length(); i++) {
+      final var word = line.substring(startIndex, i);
+      if (map.containsKey(word)) {
+        return word;
+      }
+    }
+
+    return null;
+  }
+
   private static int concatenateAndConvertToInt(Optional<Integer> first, Optional<Integer> last) {
     final var result = new StringBuilder();
     first.ifPresent(result::append);
     last.ifPresent(result::append);
 
-    return Integer.parseInt(result.toString());
+    return result.isEmpty() ? 0 : Integer.parseInt(result.toString());
   }
 }
